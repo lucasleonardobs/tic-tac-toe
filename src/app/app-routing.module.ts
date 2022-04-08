5a117a77e58e34f868d0c4c351e5a49655d2ea14
Lucas Leonardo <lucas.leonardo@g.globo>
2022-04-08 10:28:43 -0300

    Set app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
+
+const routes: Routes = [
+  {
+    path: 'game',
+    component: GameComponent,
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

287c006a5d1d37694da978d888bec76e98bd58f2
Lucas Leonardo <lucas.leonardo@g.globo>
2022-04-08 11:24:19 -0300

    Implement screen and soem styles

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.sass']
+})
+export class LoginComponent implements OnInit {
+  public loginValid = true;
+  public email = '';
+  public password = '';
+
+  public onSubmit(): void {
+    this.loginValid = true;
+  }
+
+  constructor() { };
+
+  ngOnInit(): void {
+  };
+
+}

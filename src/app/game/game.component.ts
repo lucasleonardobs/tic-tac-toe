3c45f27d260d128b6fbc56e85a6cbf15bb0b448b
Lucas Leonardo <lucas.leonardo@g.globo>
2022-04-08 09:34:46 -0300

    Create a tic-toc-game structure and initial draft

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -6,8 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game.component.sass']
 })
 export class GameComponent implements OnInit {
+  public gameState: Array<number | string> = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
   constructor() { }
+
+  public makeHumanMove(field: number): void {
+
+  }
 
   ngOnInit(): void {
   }

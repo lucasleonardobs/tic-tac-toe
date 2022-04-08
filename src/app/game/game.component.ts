6146b99b9d0ad2fc1f42ef622e971234222ca9ba
Lucas Leonardo <lucas.leonardo@g.globo>
2022-04-08 09:43:41 -0300

    Implement a Start/Reset button and Difficulty form

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -7,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
   public gameState: Array<number | string> = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  public winner: string | undefined;
+  public playing: boolean = false;
+  public computerFirst: boolean = false;
+  public difficulty: 'Easy' | 'Normal' | 'Hard' = 'Normal';
 
   constructor() { }
 
   public makeHumanMove(field: number): void {
+
+  }
+
+  public toggleGame(toggle: boolean): void {
 
   }
 
